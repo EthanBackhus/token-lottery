b8adb8835321b16cf081598930b1829cd0647fa2
Ethan Backhus <backhusethan@gmail.com>
2025-05-06 18:46:12 -0400

    first commit

---
+++ programs/token_lottery/src/lib.rs
@@ -0,0 +1,67 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+pub mod shared;
+use shared::*;
+
+declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+
+#[program]
+pub mod token_lottery {
+    use super::*;
+
+    pub fn initialize_config(ctx: Context<Initialize>, start: u64, end: u64, price: u64) -> Result<()> {
+
+        // set the bump in the account
+        ctx.accounts.token_lottery.bump = ctx.bumps.token_lottery;
+        ctx.accounts.token_lottery.start_time = start;
+        ctx.accounts.token_lottery.end_time = end;
+        ctx.accounts.token_lottery.ticket_price = price;
+        ctx.accounts.token_lottery.authority = *ctx.accounts.payer.key;
+        ctx.accounts.token_lottery.lottery_pot_amount = 0;
+        ctx.accounts.token_lottery.total_tickets = 0;
+        ctx.accounts.token_lottery.randomness_account = Pubkey::default();
+        ctx.accounts.token_lottery.winner_chosen = false;
+
+        Ok(())
+    }
+
+}
+
+
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+
+    // mutable because we are using a payer
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = payer,
+        space = ANCHOR_DISCRIMINATOR + TokenLottery::INIT_SPACE,
+        seeds = [b"token_lottery".as_ref()],
+        bump
+    )]
+    pub token_lottery: Account<'info, TokenLottery>,
+
+    pub system_program: Program<'info, System>,
+}
+
+// always save the bump in the PDA, saves compute later on
+#[account]
+#[derive(InitSpace)]
+pub struct TokenLottery {
+    pub bump: u8,
+    pub winner: u64,
+    pub winner_chosen: bool,
+    pub start_time: u64,
+    pub end_time: u64,
+    pub lottery_pot_amount: u64,
+    pub total_tickets: u64,
+    pub ticket_price: u64,
+    pub authority: Pubkey,
+    pub randomness_account: Pubkey
+}
